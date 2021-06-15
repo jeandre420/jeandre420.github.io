388730d2cf9882ad2df3e07061c923a65f5b76c1
Jeandre Stapelberg <jeandre620@outlook.com>
2021-06-15 13:32:34 +0200

    stale error update

--- stale-error/service-worker.js
+++ stale-error/service-worker.js
@@ -16,24 +16,36 @@ self.addEventListener( 'activate', function ( event )
 
 self.addEventListener( 'fetch', function ( event )
 {
+	// fetch response from network.
 	event.respondWith( fetch( event.request ).then( function ( response )
 	{
 		caches.open( cacheName ).then( function ( cache )
 		{
+			// If we receive an error response, return stale version from cache.
 			if ( response.status >= 500 )
 			{
 				cache.match( event.request ).then( function ( response )
 				{
+					// return stale version from cache.
 					return response;
-				} ).catch( function ()
+				} ) // If we don't find the stale version we return the network respons
+				.catch( function ()
 				{
 					return response;
 				} );
-			} else
+			} // If response was successfull we update the cached version.
+			else
 			{
 				cache.put( event.request, response.clone() );
 				return response;
 			}
 		} );
 	} ) );
+} );
+
+// Allows key/value pairs to be added to the current cache object. 
+// The put() method overrides any key / value pair previously stored in the cache that matches the request.
+fetch( url ).then( function ( response )
+{
+	return cache.put( url, response );
 } );
