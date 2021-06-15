1ea93b62a3e64d47ac12d12f348691b9e55a053b
Jeandre Stapelberg <jeandre620@outlook.com>
2021-06-15 11:28:47 +0200

    detail commit

---
+++ service-workers/service-worker.js
@@ -0,0 +1,9 @@
+self.addEventListener( 'install', function ( e )
+{
+	console.log( 'Install Event:', e );
+} );
+
+self.addEventListener( 'activate', function ( e )
+{
+	console.log( 'Activate Event:', e );
+} );
