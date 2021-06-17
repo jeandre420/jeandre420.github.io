63a96a2263649ddccce821b0eae7ca90e8f2cbf6
Jeandre Stapelberg <jeandre620@outlook.com>
2021-06-17 13:30:56 +0200

    offline images

--- offline-images/service-worker.js
+++ offline-images/service-worker.js
@@ -22,7 +22,7 @@ function fetchHandler( event )
 	{
 		/*return caches.match( event.request );*/
 		const cache = event.waitUntil( caches.open( cacheName ) );
-		const cachedResponse = event.waitUntil( cache.match( cacheName ));
+		const cachedResponse = event.waitUntil( cache.match( event.request ) );
 		return cachedResponse;
 	} )
 	);
