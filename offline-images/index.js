0722b7faac3e394b9964f91f68951a55b02c1177
Jeandre Stapelberg <jeandre620@outlook.com>
2021-06-17 11:20:29 +0200

    offline images

---
+++ offline-images/index.js
@@ -0,0 +1,24 @@
+// JavaScript source code
+'use-strict';
+
+var scope = {scope: './'};
+
+if ( 'serviceWorker' in navigator )
+{
+	navigator.serviceWorker.register( 'service-worker.js', scope ).then( function ( serviceWorker )
+	{
+		printStatus( 'successful' );
+	} ).catch( function ( error )
+	{
+		printStatus( error );
+	} );
+}
+else
+{
+	printStatus( 'unavailable' );
+}
+
+function printStatus( status )
+{
+	document.getElementById( 'status' ).innerHTML = status;
+}
