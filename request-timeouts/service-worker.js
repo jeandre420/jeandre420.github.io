e9e207c86057aca41e65fb1ab563bf9f4013394a
Jeandre Stapelberg <jeandre620@outlook.com>
2021-06-17 08:49:22 +0200

    request timeouts

---
+++ request-timeouts/service-worker.js
@@ -0,0 +1,40 @@
+// JavaScript source code
+function timeout( delay )
+{
+	return new Promise( function ( resolve, reject )
+	{
+		setTimeout( function ()
+		{
+			resolve( new Response( '',
+				{
+					status: 408,
+					statusText: 'Request timed out.'
+				} ) );
+		}, delay );
+	} );
+}
+
+self.addEventListener( 'install', function ( event )
+{
+	self.skipWaiting();
+} );
+
+self.addEventListener( 'activate', function ()
+{
+	if ( self.clients && clients.claim )
+	{
+		clients.claim();
+	}
+} );
+
+self.addEventListener( 'fetch', function ( event )
+{
+	if ( /\.js$/.test( event.request.url ) )
+	{
+		event.respondWith( Promise.race( [timeout( 400 ), fetch( event.request.url )] ) );
+	}
+	else
+	{
+		event.respondWith( fetch( event.request ) );
+	}
+} );
