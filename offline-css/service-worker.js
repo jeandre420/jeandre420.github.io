84238643dbf7e2974c2c5b4656ab9336c3f143fd
Jeandre Stapelberg <jeandre620@outlook.com>
2021-06-18 10:36:28 +0200

    offline css

--- offline-css/service-worker.js
+++ offline-css/service-worker.js
@@ -9,7 +9,7 @@ function installHandler( event )
 {
 	event.waitUntil( caches.open( cacheName ).then( function ( cache )
 	{
-		return cache.addAll( ['index.html', 'style-2.css'] );
+		return cache.addAll( ['index.html'] );
 	} ) );
 }
 
